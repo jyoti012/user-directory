ef034f300ba0764dd15d241e60441aa0644c3015
Jyoti Suvarna <jyoti@ajackus.com>
2020-06-30 17:06:59 +0530

    Allow user to delete record

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+ 
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:firstname, :lastname, :username, :email, :age)
