26bb1f416f7808e836fc0346e512a2108b086a29
Jyoti Suvarna <jyoti@ajackus.com>
2020-07-03 16:12:33 +0530

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_one_attached :avatar
-  validates :firstname, presence: true
-  validates :lastname, presence: true
-  validates :username, presence: true
-  validates :email, presence: true
-  validates :age, presence: true
+  validates :firstname, presence: true, length: { minimum: 2 }
+  validates :lastname, presence: true, length: { minimum: 2 }
+  validates :username, presence: true, length: { maximum: 8,
+  too_long: "%{count} characters is the maximum allowed" }
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :age, presence: true, numericality: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,39 +2,38 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   subject {
-    described_class.new(firstname: "Jyoti",
-                        lastname: "Suvarna",
-                        username: "suvarnajyoti",
-                        email: "suvarnajyoti01@gmail.cm",
-                        age: 25)
+    described_class.new(firstname: 'Jyoti',
+                        lastname: 'Suvarna',
+                        username: 'sjyoti',
+                        email: 'jyoti@ajackus.com',
+                        age: '25')
   }
 
-  it "is valid with valid attributes" do
+  it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it "is not valid without a firstname" do
-    # user = User.new(firstname: nil)
+  it 'is not valid without a firstname' do
     subject.firstname = nil
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a lastname" do
+  it 'is not valid without a lastname' do
     subject.lastname = nil
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a username" do
+  it 'is not valid without a username' do
     subject.username = nil
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a email" do
+  it 'is not valid without a email' do
     subject.email = nil
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a age" do
+  it 'is not valid without a age' do
     subject.age = nil
     expect(subject).to_not be_valid
   end
