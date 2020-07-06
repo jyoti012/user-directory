a02fcdb91adfabe841dbe36ab71bf2e0101708ed
Jyoti Suvarna <jyoti@ajackus.com>
2020-07-06 11:14:08 +0530

    Setup devise for authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http', port: 3000 }
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
