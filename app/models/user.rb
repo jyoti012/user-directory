6cbe5bc8db1967888cb9d714f7ef95311bd083eb
Jyoti Suvarna <jyoti@ajackus.com>
2020-07-06 15:35:15 +0530

    Customize devise for links and messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
-  devise :database_authenticatable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :validatable
 
   has_one_attached :avatar
   validates :firstname, presence: true, length: { minimum: 2 }
@@ -8,6 +9,6 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 8,
   too_long: "%{count} characters is the maximum allowed" }
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :encrypted_password, presence: true, length: { minimum: 6 }
+  validates :encrypted_password, presence: true, length: { minimum: 5 }
   validates :age, presence: true, numericality: true
 end
