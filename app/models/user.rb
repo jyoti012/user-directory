83776fd635f1d98b6f641e20be4760ac9a949d47
Jyoti Suvarna <jyoti@ajackus.com>
2020-07-07 13:24:27 +0530

    Do not show password

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,5 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 8,
   too_long: "%{count} characters is the maximum allowed" }
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :encrypted_password, presence: true, length: { minimum: 5 }
   validates :age, presence: true, numericality: true
 end
