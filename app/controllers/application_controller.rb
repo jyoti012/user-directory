ba989ea3fe52d7eec9451ad9c387850f2cf328f2
Jyoti Suvarna <jyoti@ajackus.com>
2020-07-07 17:07:35 +0530

    Persist selected locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   around_action :switch_locale
   before_action :authenticate_user!
-  
+  before_action :set_locale
+
   def home; end
  
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
+  end
+
+  private
+
+  def set_locale
+    @locale ||= params[:locale] || session[:locale] || I18n.default_locale
+    I18n.locale = session[:locale] = @locale
   end
 
 end
