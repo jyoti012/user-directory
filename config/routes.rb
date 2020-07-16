38607e7db81f4f93e434548edd7beef83d9262c3
Jyoti Suvarna <jyoti@ajackus.com>
2020-07-16 14:51:30 +0530

    Authenticate before showing users list

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+  get '/app', to: 'welcome#app', as: 'app'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :user
   get '*page', to: 'directory#index', constraints: ->(req) do
     !req.xhr? && req.format.html?
   end
-  root to: 'directory#index'
+  root to: 'welcome#home'
   resources :users
 end
