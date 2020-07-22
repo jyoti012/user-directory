0d2872dc59c344fcca407508a97ff6cc645b22a9
Jyoti Suvarna <jyoti.suvarna@ajackus.com>
2020-07-22 13:32:38 +0530

    [WIP]Add localization to react app

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -5,15 +5,42 @@ import Home from './Home'
 import Add from './Add'
 import Edit from './Edit'
 import View from './View'
+import { withTranslation, Trans } from 'react-i18next'
 import configureStore from '../store/configureStore';
 
 const store = configureStore();
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        value: "en"
+    }
+ }
+
+ onLanguageHandle = (event) => {
+    let newLang = event.target.value;
+    this.setState({value: newLang})
+    this.props.i18n.changeLanguage(newLang)
+  }
+
+  renderRadioButtons = () => {
+    return (
+      <div><input
+      checked={this.state.value === 'en'}
+      name="language" onChange={(e) => this.onLanguageHandle(e)} value="en" type="radio" />English &nbsp;
+      <input name="language" value="jp"
+      checked={this.state.value === 'jp'}
+      type="radio" onChange={(e) => this.onLanguageHandle(e)} />Japanese</div>
+    )
+  }
   render () {
+    const {t} = this.props
     return (
       <Provider store={store}>
         <div>
+        {t("author.title")}
+        { this.renderRadioButtons() }
           <Switch>
             <Route exact path='/' render={() => <Home/>} ></Route>
             <Route exact path='/add' render={() => <Add/>} ></Route>
@@ -26,4 +53,4 @@ class App extends React.Component {
   }
 }
 
-export default App
+export default withTranslation()(App)
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../components/App'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../i18n';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Router>
-      <Route path="/" component={App} />
+      <I18nextProvider i18n={i18n}>
+         <Route path="/" component={App}/>
+      </I18nextProvider>
     </Router>,
     document.body.appendChild(document.createElement('div')),
   )
